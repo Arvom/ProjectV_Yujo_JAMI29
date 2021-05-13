6df7df5dd7bec851566630e36777634073257427
GiuliaFrezza <83879491+GiuliaFrezza@users.noreply.github.com>
2021-05-13 12:43:08 +0200

    Add files via upload

--- src/main/java/com/yujo/dao/DaoCommentMySQL.java
+++ src/main/java/com/yujo/dao/DaoCommentMySQL.java
@@ -16,6 +16,15 @@ import com.yujo.util.IMappable;
 @Repository
 public class DaoCommentMySQL extends BasicDao implements IDaoComment{
 
+	private static final String SELECT_USERS = "SELECT * FROM users WHERE id = ?";
+	private static final String SELECT__POSTS = "SELECT name, surname, posts.* FROM posts INNER JOIN users ON users.id = posts.id_user WHERE posts.id = ?";
+	private static final String UPDATE_COMMENTS = "UPDATE comments SET content=?";
+	private static final String WHERE_ID = " WHERE id=?";
+	private static final String DELETE_COMMENTS = "DELETE FROM comments";
+	private static final String INSERT_INTO_COMMENTS = "INSERT INTO comments (id_user, id_post, content) VALUES (?,?,?)";
+	private static final String WHERE_ID_POST = " WHERE id_post = ?";
+	private static final String SELECT_COMMENTS = "SELECT * FROM comments";
+
 	public DaoCommentMySQL(
 			@Value ("${db.address}") String dbAddress,
 			@Value ("${db.user}") String user,
@@ -24,49 +33,39 @@ public class DaoCommentMySQL extends BasicDao implements IDaoComment{
 	}
 
 	private Comment setStuffInComment(int id_post, int id_user, Map<String, String> map) {
-		Post p = IMappable.fromMap(Post.class, getOne("SELECT name, surname, posts.* FROM posts INNER JOIN users ON users.id = posts.id_user WHERE posts.id = ?", id_post));
-		User u = IMappable.fromMap(User.class, getOne("SELECT * FROM users WHERE id = ?", id_user));
-		Comment c = IMappable.fromMap(Comment.class, map);
-		c.setPost(p);
-		c.setUser(u);
-		return c;
-	}
+        Post p = IMappable.fromMap(Post.class, getOne(SELECT__POSTS, id_post));
+        User u = IMappable.fromMap(User.class, getOne(SELECT_USERS, id_user));
+        Comment c = IMappable.fromMap(Comment.class, map);
+        c.setPost(p);
+        c.setUser(u);
+        return c;
+    }
 
 
-	@Override
-	public List<Comment> comments(int id_post) {
-		List<Comment> res = new ArrayList<>();
-		List<Map<String, String>> maps = getAll("SELECT * FROM comments WHERE id_post = ?", id_post);
-		for (Map<String, String> map : maps) {
-			int id_user = Integer.parseInt(map.get("id_user"));
-			res.add(setStuffInComment(id_post, id_user, map));
-		}
-		return res;
-	}
+    @Override
+    public List<Comment> comments(int id_post) {
+        List<Comment> res = new ArrayList<>();
+        List<Map<String, String>> maps = getAll(SELECT_COMMENTS+ WHERE_ID_POST, id_post);
+        for (Map<String, String> map : maps) {
+            int id_user = Integer.parseInt(map.get("id_user"));
+            res.add(setStuffInComment(id_post, id_user, map));
+        }
+        return res;
+    }
 
-	@Override
-	public boolean add(Comment c) {
-		if(c!= null){
-			return executeAndIsModified("INSERT INTO comments (id_user, id_post, content) VALUES (?,?,?)",c.getUser().getId(), c.getPost().getId(), c.getContent());
-		}else{
-			return false;
-		}
-	}
+    @Override
+    public boolean add(Comment c) {
+        return executeAndIsModified(INSERT_INTO_COMMENTS, c.getUser().getId(), c.getPost().getId(), c.getContent());
+    }
 
+    @Override
+    public boolean delete(int id) {
+        return executeAndIsModified(DELETE_COMMENTS+WHERE_ID,id);
+    }
 
-	@Override
-	public boolean delete(int id) {
-		return executeAndIsModified("DELETE FROM comments WHERE id=?",id);
-	}
-
-	@Override
-	public boolean update(Comment c) {
-		if(c!= null){
-			return executeAndIsModified("UPDATE comments SET content=? WHERE id=?", c.getContent(), c.getId());
-		}else{
-			return false;
-		}
-
-	}
+    @Override
+    public boolean update(Comment c) {
+        return executeAndIsModified(UPDATE_COMMENTS+WHERE_ID, c.getContent(), c.getId());
+    }
 
 }
