98952e6e57a3eb743dd5b59d292a1516e67c1856
Arvom <79362297+Arvom@users.noreply.github.com>
2021-05-14 12:46:59 +0200

    Add files via upload

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -14,13 +14,15 @@ $('document').ready(function () {
         renderPosts()
     }
 
-    function renderPosts(){
+    function renderPosts() {
 
         $.get('post', function (res) {
             $('#dashboard').html('');
-            for (let i = 0; i < res.length; i++) {
-                $.get('post.html', function (data) {
 
+            for (let i = 0; i < res.length; i++) {
+
+
+                $.get('post.html', function (data) {
                     $('#dashboard').append(data);
                     $('h6:contains("[USERNAME]")')
                         .text(`${res[i].name} ${res[i].surname}`)
@@ -28,7 +30,26 @@ $('document').ready(function () {
                     $('span:contains("[DATE]")').text(`${res[i].content_time}`);
                     $('p:contains("[CONTENT]")').text(`${res[i].content}`);
                     $('button[ID-POST="[ID]"]').attr('id-post', res[i].id);
+                    $('div[ID-POST="[IDPOST]"]').attr('id-post', res[i].id);
                 })
+
+                renderComments(res[i].id)
+            }
+        })
+    }
+
+    function renderComments(idPost) {
+        $.get(`comment/${idPost}`, function (res) {
+            $(`div[ID-POST="${idPost}"]`).html('');
+            if (res.length != 0) {
+                for (let i = 0; i < res.length; i++) {
+                    $.get('comment.html', function (data) {
+                        $(`div[ID-POST="${idPost}"]`).append(data);
+                        $('p:contains("[COMMENTAUTHOR]")')
+                            .text(`${res[i].user.name} ${res[i].user.surname} - ${res[i].content_time}`);
+                        $('p:contains("[COMMENTCONTENT]")').text(`${res[i].content} `);
+                    })
+                }
             }
         })
     }
@@ -77,7 +98,7 @@ $('document').ready(function () {
             }
         })
     }
-/*
+
     $('#main').on('click', '#btn-add-post', function () {
         const post = {
             content: $('#add-content').val()
@@ -115,7 +136,7 @@ $('document').ready(function () {
         $('#switch3').removeClass('active');
     })
 
-    function getProfile(){
+    function getProfile() {
         $('#main').html('');
         $.get('profile.html', function (data) {
             $('#main').append(data);
@@ -127,7 +148,7 @@ $('document').ready(function () {
         deletePost(idPost);
     })
 
-    function deletePost(idPost){
+    function deletePost(idPost) {
         $.ajax({
             url: `post/${idPost}`,
             type: 'DELETE',
@@ -147,10 +168,10 @@ $('document').ready(function () {
 
     })
 
-    function modalEditPost(idPost){
-        $.get(`post/${idPost}`, function(res){
-                $('#edit-content').text(`${res.content}`);
-                $('#btn-edit-post').attr('id-post',`${res.id}`);
+    function modalEditPost(idPost) {
+        $.get(`post/${idPost}`, function (res) {
+            $('#edit-content').text(`${res.content}`);
+            $('#btn-edit-post').attr('id-post', `${res.id}`);
         })
     }
 
@@ -162,23 +183,22 @@ $('document').ready(function () {
         editPost(post);
     })
 
-    function editPost(post){
+    function editPost(post) {
         $.ajax({
             url: 'post',
             type: 'PUT',
             data: JSON.stringify(post),
             contentType: 'application/json',
-            success: function(msg) {
-                if(msg){
+            success: function (msg) {
+                if (msg) {
                     $('#textbox').fadeOut(300);
                     $('.modal-backdrop').fadeOut(300);
                     renderPosts();
-                }else{
+                } else {
                     alert('C\'è stato un errore nel sistema')
                 }
             }
         })
     }
 
- */
 })
