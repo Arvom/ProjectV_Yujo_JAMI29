f362b7421c67349506bb3428ffe363f092b8b259
GiuliaFrezza <83879491+GiuliaFrezza@users.noreply.github.com>
2021-05-17 16:54:16 +0200

    Add files via upload

--- src/main/java/com/yujo/model/Post.java
+++ src/main/java/com/yujo/model/Post.java
@@ -5,48 +5,34 @@ import com.yujo.util.IMappable;
 public class Post implements IMappable{
 
 	private int id;
-	private String name;
-	private String surname;
-	private int id_user;
+	private User user;
 	private String content;
 	private String content_time;
+	private String image;
 	
-	public Post(int id, String name, String surname, int id_user, String content, String content_time) {
+	public Post(int id, User user, String content, String content_time, String image) {
 		super();
 		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.id_user = id_user;
+		this.user = user;
 		this.content = content;
 		this.content_time = content_time;
+		this.image = image;
 	}
-	
 	public Post() {
 		super();
 	}
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getname() {
-		return name;
+	public User getUser() {
+		return user;
 	}
-	public void setname(String name) {
-		this.name = name;
-	}
-	public String getsurname() {
-		return surname;
-	}
-	public void setsurname(String surname) {
-		this.surname = surname;
-	}
-	public int getId_user() {
-		return id_user;
-	}
-	public void setId_user(int id_user) {
-		this.id_user = id_user;
+	public void setUser(User user) {
+		this.user = user;
 	}
 	public String getContent() {
 		return content;
@@ -60,11 +46,16 @@ public class Post implements IMappable{
 	public void setContent_time(String content_time) {
 		this.content_time = content_time;
 	}
-	@Override
-	public String toString() {
-		return "{id:" + id + ", name:" + name + ", surname:" + surname + ", id_user:" + id_user + ", content:" + content
-				+ ", content_time:" + content_time + "}";
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return "{id:" + id + ", user:" + user + ", content:" + content + ", content_time:" + content_time + ", image:"
+				+ image + "}";
+	}
 }
