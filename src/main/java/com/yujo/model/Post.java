ea2eaf25027fce46945e8441cf8236582b3c473d
Arvom <79362297+Arvom@users.noreply.github.com>
2021-05-19 10:08:05 +0200

    Add files via upload

--- src/main/java/com/yujo/model/Post.java
+++ src/main/java/com/yujo/model/Post.java
@@ -6,81 +6,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 
-public class Post implements IMappable{
+public class Post implements IMappable {
 
-	static @Value("${file.upload}") String uploadDir;
+    static @Value("${file.upload}")
+    String uploadDir;
 
-	private int id;
-	private User user;
-	private String content;
-	private String content_time;
-	private String image;
+    private int id;
+    private User user;
+    private String content;
+    private String content_time;
+    private String image;
 
-	private FileStorageService fs;
 
-	@Autowired
-	public Post(FileStorageService fs) {
-		this.fs = fs;
-	}
+    public Post( int id, User user, String content, String content_time, String image ) {
+        super();
+        this.id = id;
+        this.user = user;
+        this.content = content;
+        this.content_time = content_time;
+        this.image = image;
+    }
 
-	public Post(int id, User user, String content, String content_time, String image) {
-		super();
-		this.id = id;
-		this.user = user;
-		this.content = content;
-		this.content_time = content_time;
-		this.image = image;
-	}
-	public Post() {
-		super();
-	}
+    public Post() {
+        super();
+    }
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getContent_time() {
-		return content_time;
-	}
-	public void setContent_time(String content_time) {
-		this.content_time = content_time;
-	}
-	public String getImage() {
-		return image;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setImage( MultipartFile file ) {
-		if(file == null){
-			image = null;
-		} else {
-			String fileName = fs.salvaFile(file);
-			image = uploadDir + fileName;
-		}
+    public void setId( int id ) {
+        this.id = id;
+    }
 
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setImage( String image ) {
-		this.image = image;
+    public void setUser( User user ) {
+        this.user = user;
+    }
 
-	}
+    public String getContent() {
+        return content;
+    }
 
-	@Override
-	public String toString() {
-		return "{id:" + id + ", user:" + user + ", content:" + content + ", content_time:" + content_time + ", image:"
-				+ image + "}";
-	}
+    public void setContent( String content ) {
+        this.content = content;
+    }
+
+    public String getContent_time() {
+        return content_time;
+    }
+
+    public void setContent_time( String content_time ) {
+        this.content_time = content_time;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage( String image ) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "{id:" + id + ", user:" + user + ", content:" + content + ", content_time:" + content_time + ", image:"
+                + image + "}";
+    }
 }
