4624af283d9b753a98da95d59ac38f2db785452e
Arvom <79362297+Arvom@users.noreply.github.com>
2021-05-21 00:45:35 +0200

    Add files via upload

--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -1,28 +1,171 @@
 $('document').ready(function () {
 
+    let sessionEmail = null;
+    let sessionId = null;
+
+    function renderHome() {
+        $.get('cookie', function (res) {
+            if (!res.includes('JSESSIONID')) {
+                $.get('landing_sign_log.html', function (data) {
+                    $('#css-sheet').attr('href', 'landing.css')
+                    $('body').html('');
+                    $('body').append(data);
+                })
+            } else {
+                setSessionID(res);
+
+                $.get('navbar.html', function (data) {
+                    $('#css-sheet').attr('href', 'personalized.css')
+                    $('body').html('');
+                    $('body').append(data);
+                })
+                getDashboard()
+            }
+        })
+    }
+
+    function setSessionID(res) {
+        let cookies = res.split(',')
+        let sessionCookie = null;
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i]
+            if (cookie.includes('JSESSIONID')) {
+                sessionCookie = cookie.substring(11);
+            }
+        }
+
+        $.get(`cookie/${sessionCookie}`, function (res) {
+            sessionEmail = res;
+        })
+    }
+
+    $('body').on('click', '#btn-login', function () {
+        renderLogin();
+    })
+
+    function renderLogin() {
+        $.get('login.html', function (data) {
+            $('#css-sheet').attr('href', 'login.css')
+            $('body').html('');
+            $('body').append(data);
+        })
+    }
+
+
+    $('body').on('click', '#btn-signup', function () {
+        renderSignup();
+    })
+
+    function renderSignup() {
+        $.get('signup_1.html', function (data) {
+            $('#css-sheet').attr('href', 'login.css')
+            $('body').html('');
+            $('body').append(data);
+        })
+    }
+
+    function renderSignupBis(email) {
+        $.get('signup_2.html', function (data) {
+            $('#css-sheet').attr('href', 'login.css')
+            $('body').html('');
+            $('body').append(data);
+            $('#email').val(email);
+        })
+    }
+
+    $('body').on('click', '#verify-email', function () {
+        let email = $('#email').val();
+        $.get(`user/checkEmail/${email}`, function (res) {
+            console.log(res)
+            if (res) {
+                renderSignupBis(email)
+            } else {
+                alert('L\'email inserita è già presente nel sistema');
+                renderSignup();
+            }
+        })
+    })
+
+    $('body').on('click', '#btn-submit-signup', function () {
+
+        const user = {
+            email: $('#email').val(),
+            password: $('#password').val(),
+            tax_code: $('#tax_code').val(),
+            name: $('#name').val(),
+            surname: $('#surname').val(),
+            phone: $('#phone').val(),
+            address: $('#address').val(),
+            role: "USER"
+        }
+
+        $.ajax({
+            url: '/signup',
+            type: 'POST',
+            processData: false,
+            contentType: 'false',
+            data: JSON.stringify(user),
+            contentType: 'application/json',
+            success: function (res) {
+                if (res) {
+                    renderLogin();
+                } else {
+                    alert('Codice fiscale già presente nel sistema')
+                }
+            }
+        })
+    })
+
+    // ---------  DASHBOARD ----------
+
     function getDashboard() {
 
         $.get('dashboard_main.html', function (data) {
             $('#main').html('');
             $('#main').append(data);
 
-            // TODO ID DA SESSIONE -----------------
+            $.get(`user/email/${sessionEmail}`, function (res) {
+                sessionId = res.id;
+                $('h6:contains("[USERNAME]")')
+                    .text(`${res.name} ${res.surname}`)
+                    .attr('id-author', sessionId);
+                $('#bio').text(`Telefono: ${res.phone} Indirizzo: ${res.address}`)
 
-            $('#btn-add-post')
-                .attr('id-author', 1);
+                $('#btn-add-post')
+                    .attr('id-author', sessionId);
+                renderGallery();
+                renderPosts();
+                renderSuggested();
+            })
+
         })
-        renderPosts()
+
     }
 
-    function renderHome(){
-        $.get('cookie', function(res){
-            if(res == ""){
-                $.get('landing_sign_log.html', function(data){
-                    $('#css-sheet').attr('href', 'landing.css')
-                    $('body').html('');
-                    $('body').append(data);
+    function renderSuggested() {
+        $.get('user', function (res) {
+            $('#suggested').html('')
+            for (let i = 0; i < (res.length > 5 ? 5 : res.length); i++) {
+                $.get('suggested.html', function (data) {
+                    $('#suggested').append(data)
+                    $('h6:contains("[USERNAME]")')
+                        .text(`${res[i].name} ${res[i].surname}`)
+                        .attr('id-author', sessionId);
                 })
-                // window.location.replace('/landing_sign_log.html')
+            }
+        })
+    }
+
+    function renderGallery() {
+        $.get(`post/${sessionId}`, function (res) {
+            $('#gallery').html('')
+            for (let i = 0; i < res.length; i++) {
+                if (res[i].image != null) {
+                    $.get('gallery.html', function (data) {
+                        $('#gallery').append(data)
+                        $('img[SRC="[URLIMAGE]"]').attr('src', res[i].image);
+                    })
+                }
             }
         })
     }
@@ -33,25 +176,43 @@ $('document').ready(function () {
             $('#dashboard').html('');
 
             for (let i = 0; i < res.length; i++) {
+                if (res[i].user.id == `${sessionId}`) {
+                    $.get('owned_post.html', function (data) {
+                        $('#dashboard').append(data);
+                        $('h6:contains("[USERNAME]")')
+                            .text(`${res[i].user.name} ${res[i].user.surname}`)
+                            .attr('id-author', res[i].user.id);
+                        $('span:contains("[DATE]")').text(`${res[i].content_time}`);
+                        $('p:contains("[CONTENT]")').text(`${res[i].content}`);
+                        $('button[ID-POST="[ID]"]').attr('id-post', res[i].id);
+                        $('div[ID-POST="[IDPOST]"]').attr('id-post', res[i].id);
+                        if (res[i].image != null) {
+                            $.get('img_post.html', function (data) {
+                                $(`.append-img[ID-POST="${res[i].id}"]`).append(data);
+                                $('img[SRC="[URLIMAGE]"]').attr('src', res[i].image);
+                            })
 
-                $.get('post.html', function (data) {
-                    $('#dashboard').append(data);
-                    $('h6:contains("[USERNAME]")')
-                        .text(`${res[i].user.name} ${res[i].user.surname}`)
-                        .attr('id-author', res[i].user.id_user);
-                    $('span:contains("[DATE]")').text(`${res[i].content_time}`);
-                    $('p:contains("[CONTENT]")').text(`${res[i].content}`);
-                    $('button[ID-POST="[ID]"]').attr('id-post', res[i].id);
-                    $('div[ID-POST="[IDPOST]"]').attr('id-post', res[i].id);
-                    if (res[i].image != null) {
-                        $.get('img_post.html', function (data) {
-                            $(`.append-img[ID-POST="${res[i].id}"]`).append(data);
-                            $('img[SRC="[URLIMAGE]"]').attr('src', res[i].image);
-                        })
+                        }
+                    })
+                } else {
+                    $.get('post.html', function (data) {
+                        $('#dashboard').append(data);
+                        $('h6:contains("[USERNAME]")')
+                            .text(`${res[i].user.name} ${res[i].user.surname}`)
+                            .attr('id-author', res[i].user.id);
+                        $('span:contains("[DATE]")').text(`${res[i].content_time}`);
+                        $('p:contains("[CONTENT]")').text(`${res[i].content}`);
+                        $('button[ID-POST="[ID]"]').attr('id-post', res[i].id);
+                        $('div[ID-POST="[IDPOST]"]').attr('id-post', res[i].id);
+                        if (res[i].image != null) {
+                            $.get('img_post.html', function (data) {
+                                $(`.append-img[ID-POST="${res[i].id}"]`).append(data);
+                                $('img[SRC="[URLIMAGE]"]').attr('src', res[i].image);
+                            })
 
-                    }
-                })
-
+                        }
+                    })
+                }
                 renderComments(res[i].id)
             }
         })
@@ -75,7 +236,7 @@ $('document').ready(function () {
 
     renderHome();
 
-    $('#home, #home2, #home3').click(function () {
+    $('body').on('click','#home, #home2, #home3', function () {
         getDashboard();
         $('#switch1').addClass('active');
         $('#switch1').removeClass('notification-trigger');
@@ -85,7 +246,7 @@ $('document').ready(function () {
         $('#switch3').removeClass('active');
     })
 
-    $('#user-list, #user-list2, #user-list3').click(function () {
+    $('body').on('click','#user-list, #user-list2, #user-list3', function () {
         getUserList();
         $('#switch3').addClass('active');
         $('#switch3').removeClass('notification-trigger');
@@ -95,7 +256,7 @@ $('document').ready(function () {
         $('#switch2').removeClass('active');
     })
 
-    $('#profile, #profile3').click(function () {
+    $('body').on('click','#profile, #profile3', function () {
         getProfile();
         $('#switch2').addClass('active');
         $('#switch2').removeClass('notification-trigger');
@@ -129,7 +290,7 @@ $('document').ready(function () {
     }
 
 
-    $('#main').on('click', '#btn-add-post', function () {
+    $('body').on('click', '#btn-add-post', function () {
         let form = $('#image-post')[0];
         let data = new FormData(form);
         saveImage(data)
@@ -193,7 +354,7 @@ $('document').ready(function () {
         })
     }
 
-    $('#main').on('click', '.btn-delete-post', function () {
+    $('body').on('click', '.btn-delete-post', function () {
         idPost = $(this).attr('id-post');
         deletePost(idPost);
     })
@@ -212,7 +373,7 @@ $('document').ready(function () {
         })
     }
 
-    $('#main').on('click', '.btn-edit-post', function () {
+    $('body').on('click', '.btn-edit-post', function () {
         let idPost = $(this).attr('id-post');
         modalEditPost(idPost);
 
@@ -225,7 +386,7 @@ $('document').ready(function () {
         })
     }
 
-    $('#main').on('click', '#btn-edit-post', function () {
+    $('body').on('click', '#btn-edit-post', function () {
         const post = {
             id: $(this).attr('id-post'),
             content: $('#edit-content').val()
